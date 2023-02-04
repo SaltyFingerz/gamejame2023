09a2d4d6f52fef860b3f9987de0003ab0a904972
1800997 <1800997@uad.ac.uk>
2023-02-04 18:57:39 +0000

    Added Beetroot Hopefully

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float movementConstant = 7f;
     [Range(0, 0.05f)] [SerializeField] private float movementSmoothing = .001f;
     Rigidbody playerRigidBody;
+    [SerializeField]private float maxHealth = 5;
+
+    private float CurrentHealth;
     // Start is called before the first frame update
     void Start()
     {
         playerRigidBody = GetComponent<Rigidbody>();
+        CurrentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -23,5 +27,13 @@ public class PlayerController : MonoBehaviour
         Vector3 redundantVelocity = Vector3.zero;
         playerRigidBody.velocity = Vector3.SmoothDamp(playerRigidBody.velocity, movementVector, ref redundantVelocity, movementSmoothing);
 
+    }
+    public void TakeDamage(float amount)
+    {
+        CurrentHealth -= amount;
+        if (CurrentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
